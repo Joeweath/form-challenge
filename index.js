407da12a944099be0e815d3fff83fb81a8ef9600
joeweath <jweathingtonsr@gmail.com>
2023-05-18 09:20:56 -0500

    create terms and conditions popupwindow

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+function showTerms() {
+    let overlay = document.getElementById("termsOverlay");
+    let popup = document.getElementById("termsPopup");
+    overlay.style.display = "block";
+    popup.style.display = "block";
+  }
+
+  function closeTerms() {
+    let overlay = document.getElementById("termsOverlay");
+    let popup = document.getElementById("termsPopup");
+    overlay.style.display = "none";
+    popup.style.display = "none";
+  }
